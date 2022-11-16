9ab2541a0e119f915dc9257f439fa1d6a55a803f
HarrievG <Harrie@vrknight.eu>
2022-11-16 22:10:18 +0100

    [!] Make checkmodelhierachy non-fatal

--- neo/d3xp/anim/Anim.cpp
+++ neo/d3xp/anim/Anim.cpp
@@ -1092,7 +1092,7 @@ void idMD5Anim::CheckModelHierarchy( const idRenderModel* model ) const
 		int jointNum = jointInfo[ i ].nameIndex;
 		if( modelJoints[ i ].name != animationLib.JointName( jointNum ) )
 		{
-			gameLocal.Error( "Model '%s''s joint names don't match anim '%s''s", model->Name(), name.c_str() );
+			gameLocal.Warning( "Model '%s''s joint names don't match anim '%s''s", model->Name(), name.c_str() );
 		}
 		int parent;
 		if( modelJoints[ i ].parent )
@@ -1105,7 +1105,7 @@ void idMD5Anim::CheckModelHierarchy( const idRenderModel* model ) const
 		}
 		if( parent != jointInfo[ i ].parentNum )
 		{
-			gameLocal.Error( "Model '%s' has different joint hierarchy than anim '%s'", model->Name(), name.c_str() );
+			gameLocal.Warning( "Model '%s' has different joint hierarchy than anim '%s'", model->Name(), name.c_str() );
 		}
 	}
 }
