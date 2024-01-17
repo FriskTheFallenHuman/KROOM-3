4b31b6b39ba426192c3f141f40ec9ece7d681888
Stephen Saunders <srsaunders@rogers.com>
2024-01-17 00:01:28 -0500

    Enable r_useParallelAdd* to be changed for bake* operations on multiplayer maps
    (cherry picked from commit 7bee6f2096d9d3162e3280f1c4e9e81bc307e6a2)

--- neo/renderer/tr_frontend_addmodels.cpp
+++ neo/renderer/tr_frontend_addmodels.cpp
@@ -35,7 +35,7 @@ If you have questions concerning this license or the applicable additional terms
 
 idCVar r_skipStaticShadows( "r_skipStaticShadows", "0", CVAR_RENDERER | CVAR_BOOL, "skip static shadows" );
 idCVar r_skipDynamicShadows( "r_skipDynamicShadows", "0", CVAR_RENDERER | CVAR_BOOL, "skip dynamic shadows" );
-idCVar r_useParallelAddModels( "r_useParallelAddModels", "1", CVAR_RENDERER | CVAR_BOOL, "add all models in parallel with jobs" );
+idCVar r_useParallelAddModels( "r_useParallelAddModels", "1", CVAR_RENDERER | CVAR_BOOL | CVAR_NOCHEAT, "add all models in parallel with jobs" );
 idCVar r_forceShadowCaps( "r_forceShadowCaps", "0", CVAR_RENDERER | CVAR_BOOL, "0 = skip rendering shadow caps if view is outside shadow volume, 1 = always render shadow caps" );
 // RB begin
 idCVar r_forceShadowMapsOnAlphaTestedSurfaces( "r_forceShadowMapsOnAlphaTestedSurfaces", "1", CVAR_RENDERER | CVAR_BOOL, "0 = same shadowing as with stencil shadows, 1 = ignore noshadows for alpha tested materials" );
