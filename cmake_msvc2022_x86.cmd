a6446bb1289ea20a421bf5817283eb475a08b527
Krispy <nikodash2020@hotmail.com>
2025-03-26 21:50:06 -0500

    Updated Batch Files

--- cmake_msvc2022_x86.cmd
+++ cmake_msvc2022_x86.cmd
@@ -1,7 +1,7 @@
 @echo off
 cls
-mkdir build\msvc2022-x84
-pushd build\msvc2022-x84
+mkdir build\msvc2022-x86
+pushd build\msvc2022-x86
 cmake -G "Visual Studio 17" -A Win32 %* ../../neo
 popd
 @pause
