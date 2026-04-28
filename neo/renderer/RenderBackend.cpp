5bac305d3d51ab017c197bb69beeb742f2784c87
Krispy <nikodash2020@hotmail.com>
2026-04-28 10:52:22 -0500

    Fixed missing private polygon offset.

--- neo/renderer/RenderBackend.cpp
+++ neo/renderer/RenderBackend.cpp
@@ -4162,6 +4162,7 @@ int idRenderBackend::DrawShaderPasses( const drawSurf_t* const* const drawSurfs,
 			// set privatePolygonOffset if necessary
 			if( pStage->privatePolygonOffset )
 			{
+				GL_PolygonOffset( r_offsetFactor.GetFloat(), r_offsetUnits.GetFloat() * pStage->privatePolygonOffset );
 				stageGLState |= GLS_POLYGON_OFFSET;
 			}
 
