cf64e8c6f2bef6048eb77a19bcfb6aa434dd9b56
Krispy <nikodash2020@hotmail.com>
2026-04-30 13:09:58 -0500

    Revert "Move D3XP code to its own lib"
    It breaks the SecurityCameras

--- neo/d3xp/ai/AI_Vagary.cpp
+++ neo/d3xp/ai/AI_Vagary.cpp
@@ -39,6 +39,19 @@ Vagary specific AI code
 
 #include "../Game_local.h"
 
+class idAI_Vagary : public idAI
+{
+public:
+	CLASS_PROTOTYPE( idAI_Vagary );
+
+private:
+	void	Event_ChooseObjectToThrow( const idVec3& mins, const idVec3& maxs, float speed, float minDist, float offset );
+	void	Event_ThrowObjectAtEnemy( idEntity* ent, float speed );
+};
+
+const idEventDef AI_Vagary_ChooseObjectToThrow( "vagary_ChooseObjectToThrow", "vvfff", 'e' );
+const idEventDef AI_Vagary_ThrowObjectAtEnemy( "vagary_ThrowObjectAtEnemy", "ef" );
+
 CLASS_DECLARATION( idAI, idAI_Vagary )
 EVENT( AI_Vagary_ChooseObjectToThrow,	idAI_Vagary::Event_ChooseObjectToThrow )
 EVENT( AI_Vagary_ThrowObjectAtEnemy,	idAI_Vagary::Event_ThrowObjectAtEnemy )
