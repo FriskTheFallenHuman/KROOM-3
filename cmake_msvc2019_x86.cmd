288482ffda4b50c0da567204ab09fca35d247a7a
Krispy <nikodash2020@hotmail.com>
2026-06-08 16:52:40 -0500

    Update MSVC bat file

--- cmake_msvc2019_x86.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-mkdir build\msvc2019-x86
-pushd build\msvc2019-x86
-cmake -G "Visual Studio 16" -A Win32 %* ../../neo
-popd
-@pause
--- cmake_msvc2019_x86_64.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cls
-mkdir build\msvc2019-x86_64
-pushd build\msvc2019-x86_64
-cmake -G "Visual Studio 16" -A x64 %* ../../neo
-popd
-@pause
